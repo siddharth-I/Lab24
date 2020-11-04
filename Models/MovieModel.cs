fadcbf9354bfc36a33ccc26054ab610301141bae
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2020-11-04 18:30:53 -0500

    Add files via upload

---
+++ Models/MovieModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab21.Models
+{
+    public class MovieModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public DateTime Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+
+    }
+}
